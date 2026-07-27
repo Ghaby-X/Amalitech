c9008f9f089fdc05db3e000147d6fc083894fe28
Gabriel Anyaele <gabrielanyaele21@gmail.com>
2026-07-27 15:09:40 +0200

    include dns and keypath in outputs

--- lab-dom04-AdvancedIAC/infrastructure/outputs.tf
+++ lab-dom04-AdvancedIAC/infrastructure/outputs.tf
@@ -22,3 +22,23 @@ output "instance_public_ip" {
   value       = aws_instance.web.public_ip
   description = "Public IP of the EC2 instance"
 }
+
+output "instance_public_dns" {
+  value       = aws_instance.web.public_dns
+  description = "Public DNS name of the EC2 instance"
+}
+
+output "ssh_user" {
+  value       = "ec2-user"
+  description = "SSH user for connecting to the EC2 instance (Amazon Linux default)"
+}
+
+output "key_pair_name" {
+  value       = aws_key_pair.ec2_key_pair.key_name
+  description = "Name of the AWS key pair associated with the instance"
+}
+
+output "private_key_path" {
+  value       = local_sensitive_file.private_key.filename
+  description = "Local path to the generated private key file"
+}
