d9b47c8c74e8c32f4eec1899b007031662f7cdde
Gabriel Anyaele <gabrielanyaele21@gmail.com>
2026-09-09 18:58:45 +0200

    update config, to use discrete postgres information

--- lab-dom11-microservices/app/backend/config.py
+++ lab-dom11-microservices/app/backend/config.py
@@ -6,9 +6,20 @@ import os
 # Port for flask application
 PORT = int(os.environ.get('PORT', 8000))
 
-# Database and server url
-PG_DATABASE_URL = os.environ.get('PG_DATABASE_URL', '')
-REDIS_URL = os.environ.get('REDIS_URL', '')
+PGHOST = os.environ.get('PGHOST', 'localhost')
+PGPORT = os.environ.get('PGPORT', '5432')
+PGUSER = os.environ.get('PGUSER', 'shopnow')
+PGPASSWORD = os.environ.get('PGPASSWORD', 'shopnow')
+PGDATABASE = os.environ.get('PGDATABASE', 'shopnow')
+PG_DATABASE_URL = f'postgresql://{PGUSER}:{PGPASSWORD}@{PGHOST}:{PGPORT}/{PGDATABASE}'
+
+REDIS_HOST = os.environ.get('REDIS_HOST', 'localhost')
+REDIS_PORT = os.environ.get('REDIS_PORT', '6379')
+REDIS_PASSWORD = os.environ.get('REDIS_PASSWORD', '')
+REDIS_URL = (
+    f'redis://:{REDIS_PASSWORD}@{REDIS_HOST}:{REDIS_PORT}'
+    if REDIS_PASSWORD else f'redis://{REDIS_HOST}:{REDIS_PORT}'
+)
 
 CART_COOKIE_NAME = os.environ.get('CART_COOKIE_NAME', 'cart_id')
 CART_TTL_SECONDS = int(os.environ.get('CART_TTL_SECONDS', 86400)) # last for 24 hours
