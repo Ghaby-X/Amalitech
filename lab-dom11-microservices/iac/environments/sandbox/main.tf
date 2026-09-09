65edfb3137f808cd470c790a8318428d0f14d8a1
Gabriel Anyaele <gabrielanyaele21@gmail.com>
2026-09-09 16:50:46 +0200

    wire iam and efs storage in main.tf

--- lab-dom11-microservices/iac/environments/sandbox/main.tf
+++ lab-dom11-microservices/iac/environments/sandbox/main.tf
@@ -31,3 +31,17 @@ module "security" {
   project_name = var.project_name
   vpc_id       = module.networking.vpc_id
 }
+
+module "iam" {
+  source = "../../modules/iam"
+
+  project_name = var.project_name
+}
+
+module "storage" {
+  source = "../../modules/storage"
+
+  project_name          = var.project_name
+  private_subnet_ids    = module.networking.private_subnet_ids
+  efs_security_group_id = module.security.efs_security_group_id
+}
