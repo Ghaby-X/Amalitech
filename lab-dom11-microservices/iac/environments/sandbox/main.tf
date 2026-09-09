f2a4875aaf4e6e95c7554b49d93caec6ad9798f0
Gabriel Anyaele <gabrielanyaele21@gmail.com>
2026-09-09 15:40:09 +0200

    wire security groups to main

--- lab-dom11-microservices/iac/environments/sandbox/main.tf
+++ lab-dom11-microservices/iac/environments/sandbox/main.tf
@@ -24,3 +24,10 @@ module "ecr" {
 
   project_name = var.project_name
 }
+
+module "security" {
+  source = "../../modules/security"
+
+  project_name = var.project_name
+  vpc_id       = module.networking.vpc_id
+}
