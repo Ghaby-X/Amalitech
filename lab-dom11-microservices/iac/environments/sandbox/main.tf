a58cdebac196e1c5390bc8b7b46a4d932550d27a
Gabriel Anyaele <gabrielanyaele21@gmail.com>
2026-09-10 08:22:50 +0200

    add the necessary input variables for ecs module

--- lab-dom11-microservices/iac/environments/sandbox/main.tf
+++ lab-dom11-microservices/iac/environments/sandbox/main.tf
@@ -41,3 +41,17 @@ module "storage" {
   private_subnet_ids    = module.networking.private_subnet_ids
   efs_security_group_id = module.security.efs_security_group_id
 }
+
+module "ecs" {
+  source = "../../modules/ecs"
+
+  project_name               = var.project_name
+  vpc_id                     = module.networking.vpc_id
+  public_subnet_ids          = module.networking.public_subnet_ids
+  private_subnet_ids         = module.networking.private_subnet_ids
+  alb_security_group_id      = module.security.alb_security_group_id
+  frontend_security_group_id = module.security.frontend_security_group_id
+  backend_security_group_id  = module.security.backend_security_group_id
+  postgres_security_group_id = module.security.postgres_security_group_id
+  redis_security_group_id    = module.security.redis_security_group_id
+}
