e365cf19ef8e93fc0913c5cd329baf46f380731e
Gabriel Anyaele <gabrielanyaele21@gmail.com>
2026-09-11 09:30:50 +0200

    remove redis auth

--- lab-dom11-microservices/app/backend/config.py
+++ lab-dom11-microservices/app/backend/config.py
@@ -15,11 +15,7 @@ PG_DATABASE_URL = f'postgresql://{PGUSER}:{PGPASSWORD}@{PGHOST}:{PGPORT}/{PGDATA
 
 REDIS_HOST = os.environ.get('REDIS_HOST', 'localhost')
 REDIS_PORT = os.environ.get('REDIS_PORT', '6379')
-REDIS_PASSWORD = os.environ.get('REDIS_PASSWORD', '')
-REDIS_URL = (
-    f'redis://:{REDIS_PASSWORD}@{REDIS_HOST}:{REDIS_PORT}'
-    if REDIS_PASSWORD else f'redis://{REDIS_HOST}:{REDIS_PORT}'
-)
+REDIS_URL = f'redis://{REDIS_HOST}:{REDIS_PORT}'
 
 CART_COOKIE_NAME = os.environ.get('CART_COOKIE_NAME', 'cart_id')
 CART_TTL_SECONDS = int(os.environ.get('CART_TTL_SECONDS', 86400)) # last for 24 hours
